14da29a7c5b969be1ea24af3ceba94e81c4201fd
george-bits-coder <91771684+george-bits-coder@users.noreply.github.com>
2021-12-16 21:16:03 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -13,7 +13,7 @@ router.get("/", (req, res) => {
   });
 });
 
-app.use(`/.netlify/functions/ppp`, router);
+app.use(`/.netlify/functions/api`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
