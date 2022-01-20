969f90786b5cbb85cb29b61df25187024afe2ea7
George Abraham <91771684+george-bits-coder@users.noreply.github.com>
2022-01-20 09:31:11 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -7,16 +7,51 @@ const Users = require('../models/user.model');
 const app = express();
 const router = express.Router();
 
-router.get("/sample", (req, res) => {
-  res.json({
-    hello: "koo clone api!"
-  });
+router.get('/users', async (req, res) => {
+    try{
+
+        const Allusers = await Users.find().lean().exec();
+
+        return res.send(Allusers);
+
+    } catch(err){
+        return res.status(500).send({message: err.message, status: 'failed'});
+    }
 });
 
-router.get("/p", (req, res) => {
-  res.json({
-    hello: "koo clone api!"
-  });
+router.get('/users/:id', async (req, res) => {
+    try{
+
+        const spuser = await Users.findById(req.params.id).lean().exec();
+
+        return res.send(spuser);
+
+    } catch(err){
+        return res.status(500).send({message: err.message, status: 'failed'});
+    }
+});
+router.post('/usercreate', async (req, res) => {
+    try{
+
+        const ucreate = await Users.create(req.body);
+
+        return res.status(201).send(ucreate);
+
+    } catch(err){
+        return res.status(500).send({message: err.message, status: 'failed'});
+    }
+});
+
+router.patch('users/:id', async (req, res) => {
+    try{
+
+        const upuser = await Users.findByIdAndUpdate(req.params.id, req.body, {new: true});
+
+        return res.send(upuser);
+
+    } catch(err){
+        return res.status(500).send({message: err.message, status: 'failed'});
+    }
 });
 
 app.use(`/.netlify/functions/api`, router);
