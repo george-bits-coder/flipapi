2209545b9376b42b2ca294b33be0f21032010d6e
George Abraham <91771684+george-bits-coder@users.noreply.github.com>
2022-01-20 09:23:29 +0530

    Update product.model.js

--- models/product.model.js
+++ models/product.model.js
@@ -1,17 +1,16 @@
 const {Schema,model} = require("mongoose");
 
-const productSchema = new Schema({
+const userSchema = new Schema({
     "name" : {required:true,type:String},
-    "tree" : [{required:true,type:String}],
-    "retail_price" : {required:true,type:Number},
-    "discount_price" : {required:true,type:Number},
-    "image" : [{required:true,type:String}],
-    "description" : {required:true,type:String},
-    "category" : {required:true,type:String},
-    "gender" : {required:false,type:String},
+    "password" : {required:true,type:String},
+    "email" : {required:true,type:String},
+  
+    "username" : {required:true,type:String},
+    "joineddate" : {required:true,type:String},
+   
 },{
     versionKey:false,
     timestamps:true
 });
 
-module.exports = new model("product",productSchema);
+module.exports = new model("user",userSchema);
