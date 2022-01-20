a3447abaedf8c70b9a6ad8b86e2d134204fde4c5
George Abraham <91771684+george-bits-coder@users.noreply.github.com>
2022-01-20 09:33:01 +0530

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,7 @@
 const {Schema,model} = require("mongoose");
 
 const userSchema = new Schema({
+    "id" : {required:true,type:Number},
     "name" : {required:true,type:String},
     "password" : {required:true,type:String},
     "email" : {required:true,type:String},
